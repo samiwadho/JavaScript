10f2d8f8c7b94c6c67ffc5063c1669252a4e33bb
samiwadho <96658347+samiwadho@users.noreply.github.com>
2022-08-08 23:24:48 +0500

    add files

---
+++ index2.js
@@ -0,0 +1,17 @@
+var country ="Pakistani"
+var province="Sindh"
+var distict="Qamber"
+var age=21
+var last_name="Wadho"
+
+console.log(country)
+console.log(province)
+console.log(distict)
+console.log(age)
+
+var name="Abdul Sami";
+var last_name="Wadho";
+var age=20;
+var distict="Kamber";
+
+console.log("name");

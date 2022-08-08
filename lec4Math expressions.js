be6437c5458f71c4f4b93a0bd057bac06a402837
samiwadho <96658347+samiwadho@users.noreply.github.com>
2022-08-09 02:31:49 +0500

    add files

---
+++ lec4Math expressions.js
@@ -0,0 +1,29 @@
+
+alert("Welcome to JavaScript")
+
+var name="sami"
+var isfeepaid=false
+var age=20
+//making boolean operatation to number
+isfeepaid=22
+console.log(typeof isfeepaid)
+
+console.log(typeof name)
+console.log(typeof isfeepaid)
+console.log(typeof age)
+
+
+
+var num1=2+3
+var num2=3+4
+var num3=2+6
+var num4=num1*num2/num3
+
+console.log(typeof num1)
+console.log(typeof num2)
+console.log(typeof num3)
+console.log(typeof num4)
+
+
+var a=12;var b=13;  var c=10
+console.log(a);console.log(b);console.log(c);

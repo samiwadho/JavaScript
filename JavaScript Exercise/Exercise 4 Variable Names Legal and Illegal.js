2726d3852a41ea4f2d32b4920d5351ded313c189
samiwadho <96658347+samiwadho@users.noreply.github.com>
2022-08-19 00:17:42 +0500

    add file

---
+++ JavaScript Exercise/Exercise 4 Variable Names Legal and Illegal.js
@@ -0,0 +1,93 @@
+                Smarter way to learn JavaScript Exercise 4 Variable Names Legal and Illegal
+
+
+Q1 Type the illegal character in this statement.
+var product cost = 3.45;
+Answer: product cost 
+
+Q2 Type the illegal character in this statement.
+var P2_a%88$;
+Answer:  %
+
+Q3 Rewrite this using camelCase.
+var Nameofband;
+Answer: var nameOfBand;
+
+
+Q4 Declare a variable using nothing but all of the legal characters that aren't alphabet characters or numbers.
+     Don't repeat any of them.
+Answer: var _$;
+
+Q5 Drag-and-drop. Holding down the left mouse button, drag the pieces of code onto the screen to form a statement that 
+declares a variable in camelCase.
+:Intelligence average human var ;
+
+Answer
+
+Q6 In a single statement, declare the variable firstLegalChar and assign it a string. 
+    The string consists entirely of the first character on the keyboard in the top row 
+    that's legal in a variable name, as long as the character doesn't come first.
+Answer: var firstLegalChar = "1";
+
+Q7 Declare a variable, in camelCase, that combines the two syllables boo and hoo.
+Answer: var booHoo
+
+Q8 In a single statement declare a legally-named variable and assign a number to it.
+Answer: var $num=1
+
+Q9 In a single statement declare a legally-named variable and assign to it the sum of 2 other legally-named variables.
+Answer: var greeting=_sami+$wadho;
+
+Q10 Declare a variable whose name is the combination of 3 characters, in this order: 
+    a legal first character that is not a letter, a character that would be illegal as a first character, 
+    and a character that would be legal anywhere in the name.
+Answer: var _1w;
+
+Q11 The variable boogieWoogie has already been declared. Assign to it the value of another variable, woogieBoogie.
+Answer: var boogieWoogie = woogieBoogie
+
+Q12 Declare a variable that is a combination of your first and last names. Use camelCase.
+Answer:var full_name=SamiWadho;
+
+Q13 Time goal: 20 seconds. Declare a variable using nothing but all of the legal characters 
+    that aren't alphabet characters or numbers. Don't repeat any of them.
+     (The timer starts when you click in the answer field.)
+Answer: var _$;
+
+Q14 Time goal: 20 seconds. In a single statement declare a legally-named variable and assign a number to it.
+Answer: var _sami=1;
+
+Q15 Time goal: 30 seconds. In a single statement
+     declare a legally-named variable and assign to it the sum of 2 other legally-named variables.
+Answer:  var greeting=_sami+$wadho;
+
+Q16 Time goal: 25 seconds. Declare a variable whose name is the combination of 3 characters, in this order: 
+    a legal first character that is not a letter, a character that would be illegal as a first character, 
+    and a character that would be legal anywhere in the name.
+Answer:  var _9w;
+
+Q17 Time goal: 30 seconds. The variable boogieWoogie has already been declared.
+     Assign to it the value of another variable, woogieBoogie.
+Answer var boogieWoogie = woogieBoogie
+
+Q18 Time goal: 20 seconds. Declare a variable that is a combination of your first and last names.
+     Use camelCase. If you have a complex name, simplify it.
+     Use "Sarah Billings" instead of "Sarah Sue Billings-Coopersmith III"
+Answer: var samiWadho
+
+Q19 Declare a variable whose name is your first and last names combined, in camelCase.
+    Assign the variable your first and last names, as a string.
+    Code an alert, specifying the variable, not the string, as the message.
+    Click the Run It button to run your code live.
+Answer  var name
+        name=samiWadho
+        alert(samiWadho)        
+
+
+Q20 Declare a variable that combines a number, an alphabet letter, and one of the legal characters that are neither an alphabet letter nor a number.
+    Assign to the variable a string consisting of another legal character that is neither an alphabet letter nor a number.
+    Display the variable's value in an alert, specifying the variable, not the value, as the message.
+    Click the Run It button to run your code live.
+Answer: var _1S
+        _1S=12;
+        alert(_1S);

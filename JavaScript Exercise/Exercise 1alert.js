65ef3f40f802db770b78bd6e09468ddcef1ba6b6
samiwadho <96658347+samiwadho@users.noreply.github.com>
2022-08-18 19:03:09 +0500

    add file

---
+++ JavaScript Exercise/Exercise 1alert.js
@@ -0,0 +1,77 @@
+            Smarter way to learn JavaScript exercise lec 1 Variables for Numbers
+Q1 I'll give you one exercise at a time. 
+After you give your answer for each exercise,
+ I'll give you another exercise. When you get one wrong,
+  I'll let you know. Later in the session, I'll circle back to that exercise to give you another try.
+What is the keyword that displays a message box?
+
+Answer:  alert
+
+Q2 Type the character that ends every statement.
+Answer:  ;
+
+Q3 Type the 2 characters that are missing in this statement.
+alert"You're learning JavaScript!";
+Answer:  ()
+
+Q4 A __________ ends in a semicolon.
+Answer:statement
+
+
+Q6 Click in the box and type the next character. I'll autocomplete. Don't type spaces.
+Code an alert that displays the message "Thanks for everything!"
+
+Answer:     alert("Thanks for everything!");
+
+Q7 Fail-safe coding. If you type the wrong character, I'll cancel the keystroke. Type spaces.
+Code an alert that displays the message "Boo!"
+
+Answer: alert("Boo!");
+
+Q8 The code displays a box with a message in it. Fill in the blank.
+______("You won!");
+Answer: alert
+
+Q9 The code displays a box with a message in it. Fill in the blank.
+alert("You won!____
+Answer:   ");
+
+Q10 Code an alert that displays the message "Hi"
+Answer alert("Hi");
+
+Q11 What are the 5 punctuation characters of an alert statement, in order?
+Answer:("");
+
+Q12 Code an alert statement displaying any message you like.
+Answer:alert("Hello world!");
+
+Q13 Time goal: 10 seconds. The code displays a box with a message in it. Fill in the blank.
+alert("You won!____
+Answer: ");
+
+Q14 Time goal: 20 seconds. Code an alert that displays the message "Hi"
+Answer:alert("Hi");
+
+Q15 Time goal: 20 seconds. What are the 5 punctuation characters of an alert statement, in order?
+Answer:  ("");
+
+Q16 Time goal: 25 seconds. Code an alert statement displaying any message you like.
+Answer:alert("Abdul Sami");
+
+Q17 Time goal: 25 seconds. Code an alert statement that displays your full name.
+Answer:alert("Abdul Sami Wadho");
+
+Q18 Live coding exercise: Code an alert with the message "Hello world!"
+Then click the Run It button to run your code live. Type out the code.
+If you copy and paste it from the screen showing the correct code, it may not work.
+
+Answer:alert("Hello world!");
+
+Q19 Live coding exercise: Code an alert with any message you like. Then click the Run It button to run your code live.
+ Type out the code. If you copy and paste it from the screen showing the correct code, it may not work.
+
+Answer:alert("It's been a long time");
+
+Q20 A __________ ends in a semicolon.
+Answer:statement
+

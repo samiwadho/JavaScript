7d936b694828248c5805be270a2316005d3ff1b4
samiwadho <96658347+samiwadho@users.noreply.github.com>
2022-08-18 19:32:01 +0500

    add file

---
+++ Lec_5Legal&illegel.js
@@ -0,0 +1,20 @@
+//  Legal
+var hello =1;
+var _xyq=1;
+var $work=1;
+var user1=1;
+var my$work=1;
+console.log(hello);
+console.log(_xyq);
+console.log( $work);
+console.log(user1);
+console.log(my$work);
+
+//Illegal
+
+//var 2user =12;
+//var my user=23;
+//var hello#world=12;
+//var my-infor=12;
+//var my?infor=32;
+//var my*infor=23;

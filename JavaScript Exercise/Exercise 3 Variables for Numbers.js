f71996e46b0fa6087a2da34f0f424bf80531d3d1
samiwadho <96658347+samiwadho@users.noreply.github.com>
2022-08-18 19:11:49 +0500

    add file

---
+++ JavaScript Exercise/Exercise 3 Variables for Numbers.js
@@ -0,0 +1,94 @@
+Smarter way to learn JavaScript Exercise Lec 3 Variables for Numbers
+
+Q1 You can assign a string to a variable. You can also assign a ______.
+Answer: number
+
+Q2 If a number is enclosed in quotes, it's a ______.
+Answer: string
+
+Q3 What kind of operation can be done on a number variable that, in one particular case, can't be done on a string variable?
+    Answer with a 4-letter word. (It would be a five-letter word in British usage.)
+Answer: math
+
+Q4 What is the value of orderTotal?
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+Answer: 110
+
+Q5 Drag-and-drop. Holding down the left mouse button, 
+    drag the pieces of code onto the screen to form a statement that declares a variable and assigns a value to it.
+Answer: var nicebigNumber =100000;
+
+Q6 Click in the box and type the next character. I'll autocomplete. Don't type spaces.
+   In a single statement, declare the variable numberAsString and assign the number 3 to it as a string.
+Answer: var numberAsString = "3"
+
+Q7 Fail-safe coding. If you type the wrong character, I'll cancel the keystroke. Type spaces.
+Assign to the variable caseQty, which has already been declared, the value 144.
+Answer:  caseQty = 144 ;
+
+Q8 In a single statement declare a variable and assign a number to it. 
+(You've learned only two rules for naming variables so far—no quotes and no initial number—so they're the only ones I'll hold you to.)
+Answer: var age =18;
+
+Q9 In a single statement declare a variable and assign to it the sum of 2 other variables.
+   (You've learned only two rules for naming variables so far, so they're the only ones I'll hold you to.)
+Answer: var last_name="Abdul"+"Sami"
+
+Q10 Rewrite this statement so the variable can be used in a math operation.
+var num = "9";
+Answer: var num = 9;
+
+Q11 Assign the sum of 2 numbers to a variable, which hasn't been declared beforehand. 
+    (You've learned only two rules for naming variables so far, so they're the only ones I'll hold you to.)
+Answer: var age = 12 + 6;
+
+Q12 In one statement declare a variable. In a second statement assign it the sum of 2 numbers. 
+   (You've learned only two rules for naming variables so far, so they're the only ones I'll hold you to.)
+Answer: var sum  
+        sum = 12+6
+  
+Q13  Time goal: 20 seconds. (The timer starts when you click in the answer field.)
+    In a single statement declare a variable and assign a number to it. 
+    You've learned only two rules for naming variables so far—no quotes and no initial number—so they're the only ones I'll hold you to. 
+    (The timer starts when you click in the answer field.)
+Answer: var sum =12
+
+Q14 Time goal: 25 seconds. In a single statement declare a variable and assign to it the sum of 2 other variables. 
+    (You've learned only two rules for naming variables so far, so they're the only ones I'll hold you to.)
+Answer: var first_name =sami+wadho
+
+Q15 Time goal: 20 seconds. Rewrite this statement so the variable can be used in a math operation.
+var num = "9";
+Answer: var num = 9;
+
+Q16 Time goal: 30 seconds. Assign the sum of 2 numbers to a variable, which hasn't been declared beforehand.
+     (You've learned only two rules for naming variables so far, so they're the only ones I'll hold you to.)
+Answer: var sum =12+9;
+
+Q17 Time goal: 30 seconds. In one statement declare a variable. In a second statement assign it the sum of 2 numbers. 
+    (You've learned only two rules for naming variables so far, so they're the only ones I'll hold you to.)
+Answer: var sum
+        sum =9+9;
+
+Q18 Time goal: 35 seconds. In the first statement declare a variable and assign it a number.
+    In the second statement, change the value of the variable by adding it together with a number.
+Answer: var sum =9;
+        var total= sum +9;
+
+Q19 Live coding exercise: Declare a variable and assign it a number.
+    Then code an alert, specifying the variable, not the number, as the message.
+    Click the Run It button to run your code live. Type out the code. If you copy and paste 
+    it from the screen showing the correct code, it may not work.
+Answer:  var sum = 15;
+         alert(sum);
+
+Q20 Live coding exercise:
+Declare a variable and assign it a number.
+Double the variable's value by adding the variable to itself.
+Display the variable's value in an alert, specifying the variable, not the number, as the message.
+Type out the code. If you copy and paste it from the screen showing the correct code, it may not work.
+Answer: var sum =12
+        var total =sum +12
+        alert(total);

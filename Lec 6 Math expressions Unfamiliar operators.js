45825db40e2c312df89a1e7c6effd004c481cebd
samiwadho <96658347+samiwadho@users.noreply.github.com>
2022-08-19 00:25:49 +0500

    add file

---
+++ Lec 6 Math expressions Unfamiliar operators.js
@@ -0,0 +1,52 @@
+            ///One variable Example must watch   Addition&Sub
+var a=10;
+++a;
+console.log("a=",a);
+
+var a=10;
+a++;
+console.log("a=",a);
+
+
+
+var a1=10;
+a1--;
+console.log("a1=",a1);
+
+var a1=10;
+--a1;
+console.log("a1=",a1);
+//  2 variable Addition&Sub  
+
+var a2=5;
+b=++a2;
+console.log("a2=",a2);
+console.log("b",b);
+
+var a2=7;
+b=a2++;
+console.log("a2=",a2);
+console.log("b",b);
+
+
+var a3=15;
+b1=a3--;
+console.log("b1",b1);
+console.log("a3=",a3);
+
+
+var a3=20;
+b1=--a3;
+console.log("b1",b1);
+console.log("a3=",a3);
+
+
+        //3 variable  Addition&Sub
+
+var a=10; //11=++a &10=--a
+var b=15;   //14=--b
+var c = ++a - --b + --a;
+// var c=11-14+6
+console.log("b=",b);
+console.log("a=",a);
+console.log("c=",c)

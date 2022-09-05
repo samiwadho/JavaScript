bf8619cb558a55009edf5a2a651ef0b49ffb8795
samiwadho <96658347+samiwadho@users.noreply.github.com>
2022-09-05 15:11:59 +0500

    add

---
+++ JavaScript Practice/lec3Variables_for_Numbers.js
@@ -0,0 +1,58 @@
+alert("Welcome to JavaScript")
+var age =22
+//or 
+var age
+count=22
+
+var sum = 5+17
+
+var a=4
+var b=10
+var c=8
+var d=a+b+c
+
+console.log(age)
+console.log(count)
+console.log(sum)
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+
+
+// true or False
+var isfeepaid=true;
+var examPassed=false;
+
+console.log(isfeepaid)
+console.log(examPassed)
+
+//undefined
+//undefined is variable undefined it is variable itself 
+//Difference between null and undefined
+//1 undefined means a variable has been declared bit has not yet been assigned a values
+//2 null is an assignment of no value 
+//3 undefined and null are two distinct types: is type itself(undefined)while null is an object
+//4 Unassigned variable are intialized by javaScript never sets a value to null 
+
+//Undefined
+var e;
+console.log(e)
+
+e="Assalam-e-Alaikum";
+console.log(e)
+
+///OR
+
+f="Walikum-e-Salam";
+console.log(f)
+
+f=null;
+console.log(f)
+
+//a +=5     OR     a  =a+5
+//a -=5     OR  a  =a-5
+//a  *=5    OR  a  =a*5
+//a /=5     OR  a  =a/5
+//a %=5     OR  a  =a%5
+//a **=5    OR  a  =a ** 5

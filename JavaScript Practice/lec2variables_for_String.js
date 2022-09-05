a8fa44ce65c8e3b8b3c87d2e23d5b83f2e98337c
samiwadho <96658347+samiwadho@users.noreply.github.com>
2022-09-05 15:10:58 +0500

    add

---
+++ JavaScript Practice/lec2variables_for_String.js
@@ -0,0 +1,17 @@
+var first_name="Abdul Sami "
+var last_name="Wadho"
+var age=20
+var nationality="Pakistan"
+
+var district
+distirct="Jamshoro";
+
+
+alert("Welcome to JavaScript")
+console.log(first_name)
+console.log(last_name)
+console.log(age)
+console.log(nationality)
+console.log(district);
+
+
